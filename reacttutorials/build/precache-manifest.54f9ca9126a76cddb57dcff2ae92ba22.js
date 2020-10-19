62662e651dd916121becbcc143360ec1682ea561
Jacques_Rockell <jacquesrockell@gmail.com>
2020-10-19 13:27:13 +1300

    2

---
+++ reacttutorials/build/precache-manifest.54f9ca9126a76cddb57dcff2ae92ba22.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8df1d6ee2099554bb5c65c151a84ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff9b2da270eee05b594",
+    "url": "/static/css/2.83f8d586.chunk.css"
+  },
+  {
+    "revision": "365f9b80d75044d957b2",
+    "url": "/static/css/main.03614905.chunk.css"
+  },
+  {
+    "revision": "8ff9b2da270eee05b594",
+    "url": "/static/js/2.523684e2.chunk.js"
+  },
+  {
+    "revision": "be7e90b169439398f6b074a62f3010e4",
+    "url": "/static/js/2.523684e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365f9b80d75044d957b2",
+    "url": "/static/js/main.f8e8c196.chunk.js"
+  },
+  {
+    "revision": "01cbd9dff63585231932",
+    "url": "/static/js/runtime-main.1d0bd313.js"
+  },
+  {
+    "revision": "f4a45967d20f2b130dacc5dabe708f37",
+    "url": "/static/media/FAQ.f4a45967.png"
+  },
+  {
+    "revision": "28ca15e180dc44a2f5189b3f827f67c8",
+    "url": "/static/media/HeroImg.28ca15e1.jpg"
+  },
+  {
+    "revision": "9f4343cac9bb58d32b0a9df6f69a7129",
+    "url": "/static/media/RBLogo.9f4343ca.png"
+  },
+  {
+    "revision": "dab6d6e6fbfd91aff80aa8b46b192149",
+    "url": "/static/media/downArrow.dab6d6e6.png"
+  },
+  {
+    "revision": "171d4f6b196e3b2bdd6923b7d79776ac",
+    "url": "/static/media/rblogov2.171d4f6b.png"
+  }
+]);
